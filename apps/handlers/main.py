ce020a5c118c55581cf29bc42d1874f9c3ff02c8
unknown <yolr11861@gmail.com>
2025-12-09 17:17:17 +0500

    kb

--- apps/handlers/main.py
+++ apps/handlers/main.py
@@ -1,10 +1,11 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
-from apps.keyboards.main_kb import main
+# from apps.keyboards.main_kb import main
+from apps.keyboards.admin_kb import choose,admin
 
 
 r=Router()
 
 @r.message(CommandStart())
 async def start(message):
-    await message.answer("salom",reply_markup=main)
+    await message.answer("salom",reply_markup=admin)
--- apps/keyboards/admin_kb.py
+++ apps/keyboards/admin_kb.py
@@ -1,9 +1,22 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+choose=InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Admin", callback_data="admin"),
+            InlineKeyboardButton(text="User",callback_data="user")
+        ]
+    ]
+)
+
 admin=InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            
+            InlineKeyboardButton(text="Movies",callback_data="movies"),
+            InlineKeyboardButton(text='Users',callback_data="users")
+        ],
+        [
+            InlineKeyboardButton(text="Ad",callback_data='ad')
         ]
     ]
 )
